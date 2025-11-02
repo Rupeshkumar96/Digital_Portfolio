79290b67c2c88604ad21aa1c564669b121043cc4
RupeshKumar96 <rupeshprasad5837@gmail.com>
2025-11-02 22:45:47 +0530

    change_navbar

--- app/component/Navbar1.js
+++ app/component/Navbar1.js
@@ -82,13 +82,13 @@ const Navbar1 = () => {
             </li>
             <li>
               <a
-                href="#Services1"
+                href="#Skill"
                 onClick={() => setIsOpen(false)}
                 className="block py-2 px-3 text-blue-400 rounded-sm hover:bg-gray-100 md:hover:bg-transparent 
                 md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 
                 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
-                Services
+                Skill
               </a>
             </li>
             <li>
